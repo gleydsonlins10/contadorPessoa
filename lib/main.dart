f425444730a9b58be5f5775941689c2dde473005
gleydson <gleydsonlins10@gmail.com>
2020-04-22 17:11:32 -0300

    Inclusão de imagem e comportamento dos botões de adicionar e remover pessoas

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,80 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("Pessoas: 0",
-          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold
-                          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child:  FlatButton(
-                child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child:  FlatButton(
-                child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                onPressed: () {},
-              ),
-            ),
-          ],
-        ),
-        Text("Pode Entrar!",
-          style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontSize: 30.0
-          ),
-        ),
-      ]
-    )
+    home: Home()
   ));
-
 }
+  
+  class Home extends StatefulWidget {
+    @override
+    _HomeState createState() => _HomeState();
+  }
+  
+  class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode entrar";
+
+  void _changePeople(int people) {
+      setState(() {
+        _people += people;
+
+        if(_people < 0) {
+          _infoText = "Tu tirou essa pessoa de onde?";
+        }else if(_people <= 10){
+          _infoText = "Pode entrar!";
+        }else {
+          _infoText = "Lotado";
+        }
+      });
+  }
+    @override
+    Widget build(BuildContext context) {
+      return Stack(
+                children: <Widget>[
+                Image.asset(
+                "images/restaurante.png",
+                fit: BoxFit.cover,
+                height: 1000.0,
+                ),
+                Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Text("Pessoas: $_people",
+                style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+                ),
+                ),
+                Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Padding(
+                padding: EdgeInsets.all(20.0),
+                child:  FlatButton(
+                child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+            onPressed: () { _changePeople(1);},
+            ),
+            ),
+            Padding(
+            padding: EdgeInsets.all(20.0),
+            child:  FlatButton(
+            child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+            onPressed: () { _changePeople(-1);},
+            ),
+            ),
+            ],
+            ),
+            Text(_infoText,
+            style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0
+            ),
+            ),
+            ]
+            )
+            ],
+            );
+    }
+  }
